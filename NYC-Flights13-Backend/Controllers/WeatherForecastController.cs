9177f41985b79f684ea57164fdde94f33ee63427
gundarsv <267009@viauc.dk>
2020-04-17 00:35:10 +0200

    Changes made

--- NYC-Flights13-Backend/Controllers/WeatherForecastController.cs
+++ NYC-Flights13-Backend/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace NYC_Flights13_Backend.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Hello"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
