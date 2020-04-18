24a5e4689114662f523c5661f8680e035a14be22
gundarsv <267009@viauc.dk>
2020-04-18 11:16:07 +0200

    Added comment

--- NYC-Flights13-Backend/Controllers/WeatherForecastController.cs
+++ NYC-Flights13-Backend/Controllers/WeatherForecastController.cs
@@ -11,6 +11,7 @@ namespace NYC_Flights13_Backend.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
+        // Returns some crazy weather forecast
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
