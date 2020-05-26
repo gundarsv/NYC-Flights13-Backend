7904b2ad4c9a66a0f51b960105d23ca7b61da749
gundarsv <267009@viauc.dk>
2020-05-26 20:13:24 +0200

    Small fix

--- NYC-Flights13-Backend/GrpcServices/Interfaces/IGrpcPlanesController.cs
+++ NYC-Flights13-Backend/GrpcServices/Interfaces/IGrpcPlanesController.cs
@@ -9,8 +9,6 @@ namespace NYC_Flights13_Backend.GrpcServices.Interfaces
 
         public IEnumerable<ManufacturerDTO> GetManufacturersWithMoreThan200Planes();
 
-        public IEnumerable<ManufacturerDTO> GetManufacturesWithMoreThan200Planes();
-
         public IEnumerable<NumberOfPlanesForEachManufacturerModelDTO> GetNumberOfPlanesForEachManufacturerModel(string manufacturer);
     }
 }
