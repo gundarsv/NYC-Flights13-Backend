dd48dd2df581e847190e5b5aa2836a6c7d1ad6b4
Ionel-Cristinel Putinica <ionutputinicabz@gmail.com>
2020-05-26 19:28:12 +0200

    Mean Delays + Number Of Planes For Each Manufacturer Model

--- NYC-Flights13-Backend/GrpcServices/Interfaces/IGrpcPlanesController.cs
+++ NYC-Flights13-Backend/GrpcServices/Interfaces/IGrpcPlanesController.cs
@@ -8,5 +8,9 @@ namespace NYC_Flights13_Backend.GrpcServices.Interfaces
         public IEnumerable<PlaneDTO> GetPlanes();
 
         public IEnumerable<ManufacturerDTO> GetManufacturersWithMoreThan200Planes();
+
+        public IEnumerable<ManufacturerDTO> GetManufacturesWithMoreThan200Planes();
+
+        public IEnumerable<NumberOfPlanesForEachManufacturerModelDTO> GetNumberOfPlanesForEachManufacturerModel(string manufacturer);
     }
 }
---
+++ NYC-Flights13-Backend/Models/NumberOfPlanesForEachManufacturerModelDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NYC_Flights13_Backend.Models
+{
+    public class NumberOfPlanesForEachManufacturerModelDTO
+    {
+        public string Model { get; set; }
+
+        public int NumberOfPlanes { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public NumberOfPlanesForEachManufacturerModelDTO()
+        {
+
+        }
+    }
+}
